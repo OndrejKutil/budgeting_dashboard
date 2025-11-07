72aacc09931148399b8dde79575b764458330362
Ondřej Kutil <kutil.ondrej@outlook.com>
2025-11-07 20:37:26 +0100

    frontend test

--- src/backend/backend_server.py
+++ src/backend/backend_server.py
@@ -63,7 +63,7 @@ app = FastAPI()
 # CORS configuration
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[FRONTEND_URL],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
